f33cae6daaaec0d49f90bcc3e45bd60e7c09c137
Joop Kiefte <ikojba@gmail.com>
2021-12-10 15:17:56 +0000

    Add test

---
+++ message/message_test.go
@@ -0,0 +1,18 @@
+// Test the message package
+package message_test
+
+import (
+	"testing"
+	"time"
+
+	"git.kiefte.eu/lapingvino/infodump/message"
+)
+
+// Test if the code finishes in time
+func TestMessageTimeout(t *testing.T) {
+	time.AfterFunc(10*time.Second, func() {
+		t.Error("Test timed out")
+	})
+	_ = message.New("Hello World!", 16)
+	// Success
+}
